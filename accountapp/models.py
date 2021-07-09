20b9b811bbde26d17558c2179304539b20df9aec
Ryujaewon <jewon1016@likelion.org>
2021-07-09 09:44:27 +0900

    7/7 3ban class commit 4

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+
+class NewModel(models.Model):   # 모델즈 안에 모델
+    text = models.CharField(max_length=255, null=False)  # CharField 문자필드?/null=false는 무조건 넣는단 말

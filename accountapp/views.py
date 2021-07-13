ae7fe7566b2a50effbbac5defdb793fcab3358ca
Ryujaewon <jewon1016@likelion.org>
2021-07-13 10:49:14 +0900

    7/13 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -21,3 +24,9 @@ def favorite_sports(request):
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = 

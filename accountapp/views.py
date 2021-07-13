7ac67aa5e78efa415b5db419bec5852ef2b76112
Ryujaewon <jewon1016@likelion.org>
2021-07-13 10:21:28 +0900

    7/8 3ban class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import NewModel
+
 
 def favorite_sports(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!'})
+
+        temp = request.POST.get('input_text')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
+
+        return HttpResponseRedirect(reverse('accountapp:favorite_sports'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!'})
+        data_list = NewModel.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'data_list': data_list})

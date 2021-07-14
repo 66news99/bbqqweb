2e2ca97b829d2c9a5a07a915fc074b1441ecf3eb
Ryujaewon <jewon1016@likelion.org>
2021-07-14 14:59:22 +0900

    7/14 3ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accountapp.views import favorite_sports
+from accountapp.views import favorite_sports, AccountCreateView
 
 app_name = 'accountapp'
 
-urlpatterns = {
-    path('favorite_sports/', favorite_sports, name='favorite_sports')
+urlpatterns = [
+    path('favorite_sports/', favorite_sports, name='favorite_sports'),
 
-}
+    path('create/',AccountCreateView.as_view(), name ='create'),
+]
--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
 
 from accountapp.models import NewModel
@@ -29,4 +29,5 @@ def favorite_sports(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = 
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'

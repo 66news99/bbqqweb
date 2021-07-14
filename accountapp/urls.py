b7a8fd786021bc514584801f5242f2095e2774a8
Ryujaewon <jewon1016@likelion.org>
2021-07-14 16:12:00 +0900

    7/14 3ban class commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import favorite_sports, AccountCreateView
@@ -6,6 +7,10 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('favorite_sports/', favorite_sports, name='favorite_sports'),
+
+    path('login/', LoginView.as_view(template_name = 'accountapp/login.html'), name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/',AccountCreateView.as_view(), name ='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -29,5 +29,5 @@ def favorite_sports(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:favorite_sports')
     template_name = 'accountapp/create.html'

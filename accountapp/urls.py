55f6b5b5bda00829baa71053dc5e84c3ee4db6ae
Ryujaewon <jewon1016@likelion.org>
2021-07-20 09:53:43 +0900

    7/20 3ban class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import favorite_sports, AccountCreateView, AccountDetailView
+from accountapp.views import favorite_sports, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -18,6 +18,7 @@ urlpatterns = [
 
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 ]
 
 

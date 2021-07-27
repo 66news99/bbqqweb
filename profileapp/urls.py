02c331b0443a0381d9277053e77211f8c6fad643
Ryujaewon <jewon1016@likelion.org>
2021-07-27 10:29:22 +0900

    7/27 3ban class commit 2

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from profileapp.views import ProfileCreateView
+
+app_name = 'profileapp'
+
+urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:favorite_sports')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)

b10fd72c9733aa5f06cb9c5dcdd4b80f637d4fae
Ryujaewon <jewon1016@likelion.org>
2021-07-28 16:23:10 +0900

    7/28 3ban class commit 2

--- profileapp/decorators.py
+++ profileapp/decorators.py
@@ -6,8 +6,8 @@ from profileapp.models import Profile
 def profile_ownership_required(func):
     def decorated(request, *args, **kwargs):
         target_profile = Profile.objects.get(pk=kwargs['pk'])
-        if target_profile.user ==request.user:
+        if target_profile.user == request.user:
             return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
-        return decorated
+    return decorated

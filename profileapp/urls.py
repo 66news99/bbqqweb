1ee25b702bfbd907b350e7a338e437c3d255b27d
Ryujaewon <jewon1016@likelion.org>
2021-07-29 18:03:55 +0900

    7/28 3ban class commit 3

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'profileapp'
 
 urlpatterns = [
     path('create/', ProfileCreateView.as_view(), name='create'),
-    path('update/<int:pK>', ProfileUpdateView.as_view(), name='update'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]

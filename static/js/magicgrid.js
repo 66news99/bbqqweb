10214a7a0aa1cb23e6858e7f8545b7226ddd5030
Ryujaewon <jewon1016@likelion.org>
2021-08-03 09:45:32 +0900

    8/3 3ban class commit 2

--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -282,4 +282,14 @@ let magicGrid = new MagicGrid({
   useMin: true
 });
 
+
+var masonrys = document.getElementsByTagName( "img")
+
+for (let i=0; masonrys.length; i++) {
+  masonrys[i].addEventListener('load', function () {
+    magicGrid.positionItems();
+  }, false)
+}
+
+
 magicGrid.listen();

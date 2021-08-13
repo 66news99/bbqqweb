cd885cadf052442f57aad5333a03f78df897f9b5
Ryujaewon <jewon1016@likelion.org>
2021-08-13 10:40:22 +0900

    8/13 3ban class commit 8

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
 
+from projectapp.views import ProjectCreateView
+
+app_name = 'projectapp'
 
 urlpatterns = [
-    
+    path('creat/', ProjectCreateView.as_view(), name='create')
 ]

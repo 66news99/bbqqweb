24cc1082a2cf9bd48f089f28cd74f24353accf33
Ryujaewon <jewon1016@likelion.org>
2021-08-18 16:24:58 +0900

    8/18 3ban class commit 6

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'articleapp',
     'commentapp',
     'projectapp',
+    'subscribeapp',
 ]
 
 MIDDLEWARE = [
--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+    path('subscribes/', include('subscribeapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

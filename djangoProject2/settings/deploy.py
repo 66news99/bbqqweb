06d2970846df6f7d7a9139d2ed350a7987cf3c4f
JururuK <darma4545@gmail.com>
2021-10-10 19:14:27 +0900

    10.10 새로 커밋

--- djangoProject2/settings/deploy.py
+++ djangoProject2/settings/deploy.py
@@ -17,6 +17,9 @@ SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = ["*"]
+
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
